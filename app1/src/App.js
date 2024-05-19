fa9a0c42e24cb67cc113d57944fabae3431c7647
Rishav Das <rdofficial192@gmail.com>
2024-05-19 22:13:13 +0530

    App1 created

---
+++ app1/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://github.com/r1shavd/"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+            Rishav Das (Github)
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
